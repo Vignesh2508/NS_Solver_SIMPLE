5d961ec9f2b374d221ceb006e8fcfaf7f14a5185
Vignesh2508 <vickky2508@gmail.com>
2020-08-12 11:19:51 +0530

    Update main.cpp
    Added hybrid difference scheme, increased number of grids with reduced pressure relaxation, added more comments about how to include velocity residuals.

--- main.cpp
+++ main.cpp
@@ -5,24 +5,30 @@
 //If you get NaN please decrease the number of grids to get proper solution.
 
 //Please refer to CFD book by Versteeg and Malalasekara for further info about the theory
-
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#define grid 33
+#define grid 90 //Do not increase the grid anymore, as either there will be a program crash or a NaN value due to divergence. Keep the grid lower than this.
+
+//Function to determine max of three numbers
+double GetMax(double a, double b, double c){
+    return std::max(a,std::max(b,c));
+
+}
 
 int main(void)
 {
     double aw[grid+2][grid+2], ae[grid+2][grid+2], an[grid+2][grid+2], as[grid+2][grid+2], apu[grid+2][grid+2], apv[grid+2][grid+2], app[grid+2][grid+2];
     double source[grid+2][grid+2], pressure[grid+2][grid+2], pp[grid+2][grid+2], uu[grid+2][grid+2], vv[grid+2][grid+2];
     double u[grid+2][grid+2], v[grid+2][grid+2], uold[grid+2][grid+2], vold[grid+2][grid+2], p[grid+2][grid+2];
-    double dx,dy,xmax,ymax,rho,mu,omegau,omegav,omegap,total,source_sum=0.0;
+    double dx,dy,xmax,ymax,rho,mu,omegau,omegav,omegap,total,source_sum=0.0, Fe, Fw, Fn, Fs, De, Dw, Dn, Ds;
     int i, j, t = 1, outeriter = 2000, k, vel_iter = 10, press_iter = 100;
 
     //Fluid properties
     mu = 0.01;
+
     rho = 1.0;
 
     //Length of the domain
@@ -64,7 +70,8 @@ int main(void)
     memcpy (vold, v, (grid+2)*(grid+2)*sizeof(double));
 
     //setting relaxation parameters
-    omegap = 0.3;
+    //try to keep the pressure relaxation low as this influences the solution a lot
+    omegap = 0.1;
     omegau = 0.7;
     omegav = 0.7;
 
@@ -76,11 +83,23 @@ int main(void)
         //coefficients for internal cells
         for(i=2;i<=grid;i++){
             for(j=1;j<=grid;j++){
-                ae[i][j] = std::max(- rho * dy * 0.5 * (uold[i+1][j] + uold[i][j]), 0.0) + mu*(dy/dx);
-                aw[i][j] = std::max(rho * dy * 0.5 * (uold[i][j] + uold[i-1][j]), 0.0) + mu*(dy/dx);
-                an[i][j] = std::max(-rho * dx * 0.5 * (vold[i][j+1] + vold[i-1][j+1]), 0.0) + mu*(dx/dy);
-                as[i][j] = std::max(rho * dx * 0.5 * (vold[i][j] + vold[i-1][j]), 0.0) + mu*(dx/dy);
-                apu[i][j] = ae[i][j] + aw[i][j] + an[i][j] + as[i][j];
+                Fe = rho * dy * 0.5 * (uold[i+1][j] + uold[i][j]);
+                Fw = rho * dy * 0.5 * (uold[i][j] + uold[i-1][j]);
+                Fn = rho * dx * 0.5 * (vold[i][j+1] + vold[i-1][j+1]);
+                Fs = rho * dx * 0.5 * (vold[i][j] + vold[i-1][j]);
+
+                De = mu*(dy/dx);
+                Dw = mu*(dy/dx);
+                Dn = mu*(dx/dy);
+                Ds = mu*(dx/dy);
+
+                //Hybrid differencing for discretization //Change this to convert this to a power law scheme
+                //This is devised for a 1D method by Patankar. Extending to a 2D method works to some extent
+                ae[i][j] = GetMax(-Fe, (De - (0.5*Fe)), 0.0);
+                aw[i][j] = GetMax(Fw, (Dw + (0.5*Fw)), 0.0);
+                an[i][j] = GetMax(-Fn, (Dn - (0.5*Fn)), 0.0);
+                as[i][j] = GetMax(Fs, (Ds + (0.5*Fs)), 0.0);
+                apu[i][j] = ae[i][j] + aw[i][j] + an[i][j] + as[i][j]+ (Fe-Fw) + (Fn-Fs);;
                 apu[i][j] = apu[i][j]/omegau;
             }
         }
@@ -99,11 +118,23 @@ int main(void)
         //coefficients for internal cells
         for(i=1;i<=grid;i++){
             for(j=2;j<=grid;j++){
-                ae[i][j] = std::max(- rho * dy * 0.5 * (uold[i+1][j] + uold[i+1][j-1]), 0.0) + mu*(dy/dx);
-                aw[i][j] = std::max(rho * dy * 0.5 * (uold[i][j] + uold[i][j-1]), 0.0) + mu*(dy/dx);
-                an[i][j] = std::max(-rho * dx * 0.5 * (vold[i][j] + vold[i][j+1]), 0.0) + mu*(dx/dy);
-                as[i][j] = std::max(rho * dx * 0.5 * (vold[i][j] + vold[i][j-1]), 0.0) + mu*(dx/dy);
-                apv[i][j] = ae[i][j] + aw[i][j] + an[i][j] + as[i][j];
+                Fe = rho * dy * 0.5 * (uold[i+1][j] + uold[i+1][j-1]);
+                Fw = rho * dy * 0.5 * (uold[i][j] + uold[i][j-1]);
+                Fn = rho * dx * 0.5 * (vold[i][j] + vold[i][j+1]);
+                Fs = rho * dx * 0.5 * (vold[i][j] + vold[i][j-1]);
+
+
+                De = mu*(dy/dx);
+                Dw = mu*(dy/dx);
+                Dn = mu*(dx/dy);
+                Ds = mu*(dx/dy);
+
+                //Hybrid differencing for discretization
+                ae[i][j] = GetMax(-Fe, (De - (0.5*Fe)), 0.0);
+                aw[i][j] = GetMax(Fw, (Dw + (0.5*Fw)), 0.0);
+                an[i][j] = GetMax(-Fn, (Dn - (0.5*Fn)), 0.0);
+                as[i][j] = GetMax(Fs, (Ds + (0.5*Fs)), 0.0);
+                apv[i][j] = ae[i][j] + aw[i][j] + an[i][j] + as[i][j]+ (Fe-Fw) + (Fn-Fs);;
                 apv[i][j] = apv[i][j]/omegav;
             }
         }
@@ -190,20 +221,12 @@ int main(void)
             }
         }
 
-        for(i=1;i<=grid;i++){
-            for(j=1;j<=grid;j++){
-                source[i][j] = rho*dy*(u[i+1][j]-u[i][j]) + rho*dx*(v[i][j+1]-v[i][j]);
-                source_sum = source_sum + source[i][j] * source[i][j];
-            }
-        }
+        //Use old and new velocities to calculate the residual to keep track
+        //Use norm to determine the residual
 
         //copying u and v to uold and vold
         memcpy (uold, u, (grid+2)*(grid+2)*sizeof(double));
         memcpy (vold, v, (grid+2)*(grid+2)*sizeof(double));
-
-        //total = sqrt(source_sum);
-
-        //std::cout<<"Iteration No:"<<t<<"\t"<<"Mass imbalance after correction:"<<total<<std::endl;
 
         t = t+1;
 
